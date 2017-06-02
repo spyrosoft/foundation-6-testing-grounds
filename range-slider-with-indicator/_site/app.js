9ee31b4bf90cf8cee91d19c59e48c7ee4110b8b2
Bennett Roesch <meta@spyrosoft.com>
2017-06-01 18:04:26 -0700

    Add working demo of a range slider with an indicator

---
+++ range-slider-with-indicator/_site/app.js
@@ -0,0 +1,22 @@
+$('.affects-indicator')
+	.on('changed.zf.slider', update_indicator);
+
+function update_indicator() {
+	var indicator_affect
+		= parseFloat($('.slider.affects-indicator input').val());
+	$('.with-indicator span.indicator')
+		.css('left', indicator_affect + '%');
+	
+	var with_indicator_value
+		= parseFloat($('.slider.with-indicator input').val());
+	var slider_fill_left_percent;
+	var slider_fill_width_percent = Math.abs(with_indicator_value - indicator_affect);
+	if (with_indicator_value < indicator_affect) {
+		slider_fill_left_percent = with_indicator_value;
+	} else {
+		slider_fill_left_percent = indicator_affect;
+	}
+	$('.slider.with-indicator span.slider-fill')
+		.css('left', slider_fill_left_percent + '%')
+		.css('width', slider_fill_width_percent + '%');
+}
